03dba10236add9517c8e6065d1a15fb82e280b3c
silvajpedro <belojpedro.31@gmail.com>
2022-06-29 21:16:18 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,60 @@
+// -Comedy array
+let comedy = [{
+    title: "Going in Style",
+    director: "Zach Braff",
+    year: "2017",
+},
+{
+    title: "My Mom Is a Character ",
+    director: "André Pellenz",
+    year: "2013",
+},
+{
+    title: "A Dog´s Will",
+    director: "Guel Arraes",
+    year: "2000",
+}
+]
+// -Action array
+let action = [{
+    title: "Bad Boys",
+    director: "Michael Bay",
+    year: "1995",
+},
+{
+    title: "THE EQUALIZER",
+    director: "Antoine Fuqua",
+    year: "2014",
+},
+{
+    title: "John Wick",
+    director: "Chad Stahelski",
+    year: "2014",
+}
+]
+// -Fantasy array
+let fantasy = [{
+    title: "The Lord of The Rings: The Return of the King",
+    director: "Peter Jackson",
+    year: "2003",
+},
+{
+    title: "The League of Extraordinary Gentlemen",
+    director: "Stephen Norrington",
+    year: "2003",
+},
+{
+    title: "Clash of the Titans",
+    director: "Louis Leterrier",
+    year: "2010",
+}
+]
+
+//*Extra Challenge*
+ if(fantasy[0].title == "The Lord of The Rings: The Return of the King" && fantasy[1].title == "The League of Extraordinary Gentlemen" && fantasy[2].title =="Clash of the Titans"){
+    console.log("You have a very good taste on movies")
+    console.log(`My favorite movie is Patch Addams ;D`)
+ } else {
+    console.log("You may have a good taste on movies, but not with this eccentricity =P (just kidding ;D) ")
+    console.log("NOT EVEN 5 STARS...")
+ }
